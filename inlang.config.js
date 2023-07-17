15123eaae4b72197c641994f2f3a34181723bb10
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-07-17 14:36:34 +0200

    Create inlang.config.js

---
+++ inlang.config.js
@@ -0,0 +1,22 @@
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
+export async function defineConfig(env) {
+	const { default: jsonPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@3/dist/index.js",
+	)
+
+	const { default: standardLintRules } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
+	)
+
+	return {
+		referenceLanguage: "en",
+		plugins: [
+			jsonPlugin({
+				pathPattern: "shared/i18n/locales/{language}/translation.json",
+			}),
+			standardLintRules(),
+		],
+	}
+}
